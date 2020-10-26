d0cc311beeba0a4f0e7c4b3a374b03d3750e5060
Nathan ter Bogt <nathan_terbogt@flightcentre.com>
2020-10-26 17:05:37 +1000

    Improvements on entity access call.

--- src/Controller/AllowedLanguagesController.php
+++ src/Controller/AllowedLanguagesController.php
@@ -26,27 +26,29 @@ class AllowedLanguagesController extends ContentTranslationController {
     $build = parent::overview($route_match, $entity_type_id);
     $user = $this->currentUser();
 
-    if (!$user->hasPermission('translate all languages') &&
-      !empty($build['content_translation_overview']['#rows'])) {
-      $rows = &$build['content_translation_overview']['#rows'];
-      $languages = $this->languageManager()->getLanguages();
-      $allowed_languages = \Drupal::service('allowed_languages.allowed_languages_manager')->assignedLanguages();
-      // Index of a row with the language in the parent output.
-      $i = 0;
-      // Parent overview() method does the same loop through available languages.
-      foreach ($languages as $language) {
-        // If the user is not allowed to manage entities in this language.
-        if (!in_array($language->getId(), $allowed_languages)) {
-          $target_row = $rows[$i];
-          // Row with operations will always be the last. See parent method.
-          end($target_row);
-          $operations_key = key($target_row);
-          // Unset operations element in case if user can't edit entities in this language.
-          unset($rows[$i][$operations_key]['data']);
-        }
-        // Increment the language index.
-        $i++;
+    if ($user->hasPermission('translate all languages') ||
+      empty($build['content_translation_overview']['#rows'])) {
+      return $build;
+    }
+
+    $rows = &$build['content_translation_overview']['#rows'];
+    $languages = $this->languageManager()->getLanguages();
+    $allowed_languages = \Drupal::service('allowed_languages.allowed_languages_manager')->assignedLanguages();
+    // Index of a row with the language in the parent output.
+    $i = 0;
+    // Parent overview() method does the same loop through available languages.
+    foreach ($languages as $language) {
+      // If the user is not allowed to manage entities in this language.
+      if (!in_array($language->getId(), $allowed_languages)) {
+        $target_row = $rows[$i];
+        // Row with operations will always be the last. See parent method.
+        end($target_row);
+        $operations_key = key($target_row);
+        // Unset operations element in case if user can't edit entities in this language.
+        unset($rows[$i][$operations_key]['data']);
       }
+      // Increment the row index.
+      $i++;
     }
 
     return $build;
